e3c867bfb11332b2118405272d892b9936c349b9
Tran Thanh Dan <thanhdan@jv-it.com.vn>
2021-02-26 08:39:11 +0700

    add zalo chat

--- source/resources/views/layouts/home.blade.php
+++ source/resources/views/layouts/home.blade.php
@@ -38,6 +38,9 @@
             </div>
         </div>
     </div>
+    <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+
+    <script src="https://sp.zalo.me/plugins/sdk.js"></script>
     <style>
         .fone {
             font-size: 19px;
